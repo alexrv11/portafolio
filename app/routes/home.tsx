f1619a876976866461fbccf47f3b78c6e21ce925
Alex Ventura <alex.rv11@gmail.com>
2025-06-16 06:37:26 -0300

    feat: adjustment on sidebar, wrapper content and bg color

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,5 @@
-import { ThemeButton } from "~/components/ThemeButton";
 import type { Route } from "./+types/home";
-import { useState } from "react";
 import { motion } from "framer-motion";
-import { LinkedInIcon } from "~/icons/linkein";
-import { PdfIcon } from "~/icons/pdf";
-import { GithubIcon } from "~/icons/github";
-import { SkillLabel } from "~/components/SkillLabel";
-import { div } from "framer-motion/client";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -16,16 +9,10 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  const [isDark, setIsDark] = useState<boolean>(true);
-    const onHandlerChangeTheme = () => {
-      console.log("Change theme", isDark);
-      setIsDark((value: boolean) => !value);
-    };
   
     return (
-      <div className={isDark ? "dark h-full" : "h-full"}>
-        <div className=" bg-white dark:bg-slate-950 h-full flex justify-center  text-gray-800 dark:text-violet-100 transition ease-in-out duration-1000">
-          <ThemeButton onClick={onHandlerChangeTheme} />
+        <div className=" bg-white  h-full flex justify-center transition ease-in-out duration-1000">
+          
           <div className="flex flex-col justify-center items-center">
             <div className="flex flex-row">
               <motion.div
@@ -49,53 +36,11 @@ export default function Home() {
               </motion.div>
             </div>
             <div>
-              <h2 className="text-xl tracking-widest font-semibold text-gray-800 dark:text-slate-200 mt-6">
+              <h2 className="text-xl tracking-widest font-semibold  mt-6">
                 Senior Software Engineer
               </h2>
             </div>
-            <div className="flex mt-4 justify-around">
-              <SkillLabel text="Go" />
-              <SkillLabel text="Typescript" />
-              <SkillLabel text="AWS" />
-              <SkillLabel text="Reactjs" />
-            </div>
-            <div className="flex mt-1 justify-around">
-              <SkillLabel text="Docker" />
-              <SkillLabel text="Serverless AWS" />
-              <SkillLabel text="Nodejs" />
-              <SkillLabel text="Nextjs" />
-            </div>
-            <div className="flex flex-row mt-8">
-              <a
-                className="mr-4 ml-4  flex flex-col items-center group"
-                href="https://github.com/alexrv11"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <GithubIcon />
-                <span className="mt-2 group-hover:text-blue-400">Github</span>
-              </a>
-              <a
-                className="mr-4 ml-4  flex flex-col items-center group"
-                href="https://www.linkedin.com/in/alexventuraq/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <LinkedInIcon />
-                <span className="mt-2 group-hover:text-blue-400">LinkedIn</span>
-              </a>
-              <a
-                className="mr-4 ml-4 flex flex-col items-center group"
-                href="https://drive.google.com/file/d/1-6pxrwaK314VxAITfctMo1RDacjsWJgQ/view?usp=sharing"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <PdfIcon />
-                <span className="mt-2 group-hover:text-blue-400">Resume</span>
-              </a>
-            </div>
           </div>
         </div>
-      </div>
     );
 }
