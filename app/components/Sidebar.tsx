81895622e6fd002de70c8c9cf90314085594adc2
Alex Ventura <alex.rv11@gmail.com>
2025-06-16 18:07:18 -0300

    fix: nav link after click it hides the menu

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -10,16 +10,20 @@ interface SidebarProps {
 interface SideBarLinkProps {
   to: string;
   children: React.ReactNode;
+  handleClick?: () => void;
 }
 
-function SideBarLink({ to, children }: SideBarLinkProps) {
+function SideBarLink({ to, children, handleClick }: SideBarLinkProps) {
   const location = useLocation();
   const isActive = location.pathname === to;
+  
+
   return (
     <li className="transform transition-opacity ease-in-out duration-[750ms] mt-[1.5em] opacity-100">
       <NavLink
         to={to}
-        className={`block text-[rgba(255,255,255,0.35)] text-xs font-bold tracking-widest leading-[1.75] uppercase hover:text-[rgba(255,255,255,0.55)] active:text-white transition-colors ease-in-out duration-200 border-0 outline-0 p-[1.35em_0] relative text-decoration-none before:content-[''] before:absolute before:bottom-0 before:right-0 before:w-full before:h-[0.2em] before:bg-[#3c2c62] hover:before:bg-[rgba(255,255,255,0.55)] ${isActive ? "text-white" : ""}`}
+        className={`block text-[rgba(255,255,255,0.35)] md:text-base text-lg font-bold tracking-widest leading-[1.75] uppercase hover:text-[rgba(255,255,255,0.55)] active:text-white transition-colors ease-in-out duration-200 border-0 outline-0 p-[1.35em_0] relative text-decoration-none before:content-[''] before:absolute before:bottom-0 before:right-0 before:w-full before:h-[0.2em] before:bg-[#3c2c62] hover:before:bg-[rgba(255,255,255,0.55)] ${isActive ? "text-white" : ""}`}
+        onClick={() => handleClick && handleClick()} // Close sidebar on click
       >
         {children}
       </NavLink>
@@ -45,10 +49,10 @@ export default function Sidebar({ className }: SidebarProps) {
         <div className="inner flex flex-col justify-center min-h-full opacity-100 w-full transition-opacity ease-in-out duration-1000">
           <nav>
             <ul className="list-none p-0">
-              <SideBarLink to="/">Home</SideBarLink>
-              <SideBarLink to="/about">About</SideBarLink>
-              <SideBarLink to="/services">Services</SideBarLink>
-              <SideBarLink to="/contact">Contact</SideBarLink>
+              <SideBarLink to="/" handleClick={() => setIsOpen(false)}>Home</SideBarLink>
+              <SideBarLink to="/about" handleClick={() => setIsOpen(false)}>About</SideBarLink>
+              <SideBarLink to="/services" handleClick={() => setIsOpen(false)}>Services</SideBarLink>
+              <SideBarLink to="/contact" handleClick={() => setIsOpen(false)}>Contact</SideBarLink>
             </ul>
           </nav>
         </div>
