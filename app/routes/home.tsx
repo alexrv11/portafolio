c97395863e4a18bbbdb6290f9e2eacd62f85f7c3
Alex Ventura <alex.rv11@gmail.com>
2025-06-16 22:19:19 -0300

    feat: add style to the home content

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -64,19 +64,22 @@ export default function Home() {
               stiffness: 300,
               damping: 20,
             }}
-            className="font-bold md:text-8xl md:mt-2 mt-20 text-4xl" // Added responsive text size adjustment
+            className="font-bold md:text-8xl md:mt-2 mt-20 text-4xl text-[#312450]" // Added responsive text size adjustment
           >
             Alex Ventura
           </motion.div>
         </div>
         <div>
-          <h2 className="md:text-3xl text-xl tracking-widest font-semibold mt-4 mb-4 text-center md:text-left">
+          <h2 className="md:text-3xl text-xl tracking-widest font-semibold mt-4 mb-4 text-center md:text-left text-[#312450]">
             <span className="hidden md:inline">{title}</span>
             <span className="inline md:hidden">{shortTitle}</span>
           </h2>
         </div>
       </div>
-      <div id="content" className="flex flex-col items-center md:mt-10 mt-1 mx-4">
+      <div
+        id="content"
+        className="flex flex-col items-center md:mt-10 mt-1 mx-4"
+      >
         <div className="max-w-2xl">
           <motion.p
             initial={{ opacity: 0 }}
@@ -92,19 +95,22 @@ export default function Home() {
           </motion.p>
           <div className="flex justify-end mt-6">
             <motion.button
-              initial={{ opacity: 0 }}
+              initial={{ opacity: 1 }}
               animate={{ opacity: 1 }}
-              transition={{ duration: 1, delay: 1.6 }}
-              className="mt-2 px-6 py-4 bg-[#312450] text-white font-bold rounded-3xl hover:bg-[#3c2c62] flex items-center"
+              whileHover={{ backgroundColor: "#064e3b" }}
+              transition={{ duration: 0.5, ease: [0.77, 0, 0.175, 1] }}
+              className="group pointer-events-auto relative flex h-fit w-fit items-center justify-center overflow-hidden rounded-full bg-[#312450] px-6 py-2 font-bold uppercase tracking-wide text-white text-base"
             >
+              <span className="absolute inset-0 bg-green-800 translate-y-full transition-all duration-500 ease-[cubic-bezier(0.77,0,0.175,1)] group-hover:translate-y-0 group-hover:rounded-none"></span>
+
               <NavLink
                 to="/contact"
                 className="text-white no-underline uppercase flex items-center"
               >
-                <span className="mr-2">
+                <span className="relative z-10 inline-flex items-center space-x-2">
                   <ArrowUpLeftIcon />
+                  <span>CONTACT</span>
                 </span>
-                CONTACT
               </NavLink>
             </motion.button>
           </div>
